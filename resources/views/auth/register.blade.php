d4a2705976226a76933417350d146af3aa3b33a0
juanangelaalma <juanlah77@gmail.com>
2022-07-03 22:13:27 +0700

    create register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,69 +1,76 @@
 <x-guest-layout>
     <div class="flex flex-col overflow-y-auto md:flex-row">
-        <div class="h-32 md:h-auto md:w-1/2">
+        <div class="hidden md:block h-32 p-6 md:h-auto md:w-1/2">
             <img aria-hidden="true" class="object-cover w-full h-full"
-                 src="{{ asset('images/create-account-office.jpeg') }}" alt="Office"/>
+                 src="{{ asset('svg/register.svg') }}"
+                 alt="Office"/>
         </div>
 
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
             <div class="w-full">
-                <h1 class="mb-4 text-xl font-semibold text-gray-700">
-                    Create account
-                </h1>
+                <div class="flex flex-row justify-start items-end mb-3">
+                    <h1 class="text-user-lg font-semibold text-primary-index">
+                        DAFTAR
+                    </h1>
+                    <img src="{{ asset('svg/brand.svg') }}" alt="Bumil Sehat">
+                </div>
+
+                <p class="text-dark text-base mb-3 font-light">Masukkan data diri anda untuk mendaftar</p>
 
                 <x-auth-validation-errors :errors="$errors"/>
+
+                <hr>
 
                 <form method="POST" action="{{ route('register') }}">
                 @csrf
 
                     <div class="mt-4">
-                        <x-label for="name" :value="__('Name')"/>
-                        <x-input type="text"
+                        <x-label-user for="name" :value="__('Nama Lengkap')"/>
+                        <x-input-user type="text"
                                  id="name"
                                  name="name"
                                  class="block w-full"
                                  value="{{ old('name') }}"
                                  required
+                                 placeholder="Masukkan nama lengkap anda"
                                  autofocus/>
                     </div>
 
                     <div class="mt-4">
-                        <x-label for="email" :value="__('Email')"/>
-                        <x-input name="email"
+                        <x-label-user for="email" :value="__('Alamat Email')"/>
+                        <x-input-user name="email"
                                  type="email"
                                  class="block w-full"
+                                 placeholder="Masukkan alamat email anda"
                                  value="{{ old('email') }}"/>
                     </div>
 
                     <div class="mt-4">
-                        <x-label for="password" :value="__('Password')"/>
-                        <x-input type="password"
+                        <x-label-user for="password" :value="__('Password')"/>
+                        <x-input-user type="password"
                                  name="password"
                                  class="block w-full"
+                                 placeholder="Masukkan password"
                                  required/>
                     </div>
 
                     <div class="mt-4">
-                        <x-label id="password_confirmation" :value="__('Confirm Password')"/>
-                        <x-input type="password"
+                        <x-label-user id="password_confirmation" :value="__('Konfirmasi Password')"/>
+                        <x-input-user type="password"
                                  name="password_confirmation"
                                  class="block w-full"
+                                 placeholder="Masukkan konfirmasi password"
                                  required/>
                     </div>
 
                     <div class="mt-4">
-                        <x-button class="block w-full">
-                            {{ __('Register') }}
-                        </x-button>
+                        <x-button-user class="block w-full">
+                            {{ __('DAFTAR') }}
+                        </x-button-user>
                     </div>
                 </form>
 
-                <hr class="my-8"/>
-
-                <p class="mt-4">
-                    <a class="text-sm font-medium text-primary-600 hover:underline"
-                       href="{{ route('login') }}">{{ __('Already registered?') }}</a>
-                </p>
+                <p class="text-dark text-base mt-3 font-light">Sudah mempunyai akun? <a class="text-secondary font-medium text-md" href="/login">Masuk</a> </p>
             </div>
         </div>
 </x-guest-layout>
--- resources/views/components/input-user.blade.php
+++ resources/views/components/input-user.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 h-[49px] border-gray-300 rounded-lg shadow-sm focus:border-primary-index focus:ring focus:ring-opacity-50 focus:ring-1 focus:ring-primary-index focus-within:text-primary-index']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 h-[49px] border-gray-300 rounded-lg shadow-sm focus:border-primary-index placeholder:text-light placeholder:font-light focus:ring focus:ring-opacity-50 focus:ring-1 focus:ring-primary-index focus-within:text-primary-index']) !!}>
