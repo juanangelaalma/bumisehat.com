bb28f3c3d3f50d3c3e519ff5f1b4c594b3d8de82
juanangelaalma <juanlah77@gmail.com>
2022-07-05 21:15:31 +0700

    add title for home fetures

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <x-carousel></x-carousel>
+    <div class="py-10">
+        <x-title-section title="SELAMAT DATANG DI BUMIL SEHAT" desc="kami menawarkan beberapa fitur yang bisa anda gunakan" line={{ true }}></x-title-section>
     <div class="px-4 py-10 md:px-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div class="flex items-center w-full bg-white px-4 py-10 h-[25rem] md:h-[20rem]">
             <div class="flex flex-col items-center"> 
@@ -67,6 +69,7 @@
             </div>
         
         </div>
+    </div>
         {{-- <x-card-feature></x-card-feature>
         <x-card-feature></x-card-feature>
         <x-card-feature></x-card-feature> --}}
