b522dc7b5f573c6ab444a0bbeb7e7013e5701cc9
juanangelaalma <juanlah77@gmail.com>
2022-07-07 22:27:26 +0700

    show data about

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\About;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     public function about() {
-        return view('about');
+        return view('about', ['about' => About::get('body')->first()->body]);
     }
 
     public function contact() {
-        return view('contact');
+        return view('contact', ['contact' => About::get(['email', 'whatsapp'])->first()]);
     }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="bg-white py-14">
   <x-title-header-user title="Tentang"></x-title-header-user>
-  <div class="text-dark text-user-base px-3 leading-8 md:px-24 py-5">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus atque dicta porro eaque iure iste, ut excepturi ipsam adipisci reprehenderit illo nobis odio voluptatibus? Inventore possimus doloremque esse expedita eius reprehenderit obcaecati, nisi, numquam ducimus dolore officia dolorum dicta iusto est. Dolorum quasi nihil harum ducimus veniam impedit numquam aspernatur, dicta commodi. Iste repellendus cumque ut, aliquid est laudantium suscipit praesentium officiis fuga possimus unde sapiente similique aut minus aliquam accusamus id magnam totam! Porro quis suscipit rem illo alias quibusdam, tempora magni possimus voluptatem incidunt, culpa reiciendis impedit. Optio minima cum quod suscipit pariatur quasi quidem nostrum maxime explicabo.
+  <div class="text-dark text-user-base px-3 leading-8 md:px-48 py-5">
+    {{ $about }}
   </div>
 </div>
 @endsection
