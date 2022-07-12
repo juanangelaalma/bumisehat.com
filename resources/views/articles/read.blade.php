c3eacca15ca0dd48e01f7b130e23da6a5612b767
juanangelaalma <juanlah77@gmail.com>
2022-07-12 23:22:40 +0700

    change padding:

--- resources/views/articles/read.blade.php
+++ resources/views/articles/read.blade.php
@@ -6,10 +6,10 @@
 @endphp
 <div class="bg-white py-14">
   <x-title-header-user title="{{ $article->title }}"></x-title-header-user>
-  <div class="w-full px-3 md:px-24 mb-2 mt-4 md:mb-4 md:mt-8">
+  <div class="w-full px-3 md:px-24 my-2 mt-4 md:my-4 md:mt-8">
     <img class="w-full shadow-sm" src="{{ asset("storage/uploads/articles/thumbnails/$article->image") }}" alt="">
   </div>
-  <div class="text-dark-gray text-user-base px-3 md:px-24 py-5">
+  <div class="text-dark-gray text-user-base px-3 md:px-24 py-3">
     {!! $article->body !!}
   </div>
 </div>
