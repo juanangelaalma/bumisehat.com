956be5f81748e61c5d31d6bde402a3a12e80b65f
juanangelaalma <juanlah77@gmail.com>
2022-07-19 20:41:31 +0700

    add animation to progress

--- resources/views/components/btn-todo.blade.php
+++ resources/views/components/btn-todo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-end space-x-2 w-full items-center">
-  <span class="text-base md:text-base text-dark-gray font-normal">Apakah sudah dilakukan?</span>
+  <span class="text-sm md:text-base text-dark font-semibold">Apakah sudah dilakukan?</span>
   <form action="{{ route('pregnancy.done', $id) }}" method="POST">
     @csrf
     <button type="submit" class="px-7 py-3 bg-primary-index truncate rounded-2xl text-white hover:bg-primary-dark transition-colors duration-300 cursor-pointer">😁 Sudah</button>
--- resources/views/components/pregnancy-progress.blade.php
+++ resources/views/components/pregnancy-progress.blade.php
@@ -1,3 +1,19 @@
+<style>
+    @keyframes widthScale {
+        0% {
+            transform: translateX(-100%);
+        }
+
+        100% {
+            transform: translateX(0);
+        }
+    }
+
+    .progress_animate {
+        animation: 1.5s ease-in-out 0.3s 1 widthScale;
+    }
+</style>
+
 <div class="md:px-10 flex flex-row space-x-1 md:space-x-6 shadow-md px-3 py-4 rounded-lg border border-gray-100">
     <div class="flex flex-col justify-start items-center">
         <span class="text-user-sm md:text-base text-dark">Minggu</span>
@@ -5,8 +21,9 @@
     </div>
 
     <div class="flex flex-row w-full items-center">
-        <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-            <div class="bg-primary-index h-2.5 rounded-full" style="width: {{ $age / 42 * 100 }}%"></div>
+        <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 overflow-hidden">
+            <div class="bg-primary-index h-2.5 rounded-full progress_animate" style="width: {{ ($age / 42) * 100 }}%;">
+            </div>
         </div>
     </div>
 
