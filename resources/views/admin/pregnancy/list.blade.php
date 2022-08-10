1e2699144bac47af8444885c91684690450ff452
juanangelaalma <juanlah77@gmail.com>
2022-08-10 23:00:37 +0700

    remove wrapper

--- resources/views/admin/pregnancy/list.blade.php
+++ resources/views/admin/pregnancy/list.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
-  <x-slot name="header">
-    <div class="flex items-center justify-between nowrap w-full">
-        {{ __('Daftar Pengingat') }}
-        <x-link href="{{ route('admin.pregnancy_alerts.list.create') }}" class="px-10"><span class="font-bold">{{ __('+ ') }}</span> Buat pengingat baru</x-link>
-      </div>
-  </x-slot>
+    <x-slot name="header">
+        <div class="flex items-center justify-between nowrap w-full">
+            {{ __('Daftar Pengingat') }}
+            <x-link href="{{ route('admin.pregnancy_alerts.list.create') }}" class="px-10"><span
+                    class="font-bold">{{ __('+ ') }}</span> Buat pengingat baru</x-link>
+        </div>
+    </x-slot>
 
-    <div class="p-4 bg-white rounded-lg shadow-xs">
-      <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
+    <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
         <div class="w-full overflow-x-auto">
             <table class="w-full whitespace-no-wrap">
                 <thead>
@@ -26,16 +26,16 @@
                                 <div class="flex items-center text-sm">
                                     <!-- Avatar with inset shadow -->
                                     <div class="relative w-8 h-8 mr-3 rounded-full md:block">
-                                        @if($pregnancy->image)
-                                          <img class="object-cover w-full h-full rounded-full"
-                                          onerror="pictureError(event, this)"
-                                          noimageshandler={{ asset("images/broken/no-pictures.png") }}
-                                          src="{{ asset("storage/uploads/pregnancy_alerts/thumbnails/$pregnancy->image") }}" alt=""
-                                          loading="lazy" />
+                                        @if ($pregnancy->image)
+                                            <img class="object-cover w-full h-full rounded-full"
+                                                onerror="pictureError(event, this)"
+                                                noimageshandler={{ asset('images/broken/no-pictures.png') }}
+                                                src="{{ asset("storage/uploads/pregnancy_alerts/thumbnails/$pregnancy->image") }}"
+                                                alt="" loading="lazy" />
                                         @else
-                                          <img class="object-cover w-full h-full rounded-full"
-                                          src="{{ asset("images/broken/no-pictures.png") }}" alt=""
-                                          loading="lazy" />
+                                            <img class="object-cover w-full h-full rounded-full"
+                                                src="{{ asset('images/broken/no-pictures.png') }}" alt=""
+                                                loading="lazy" />
                                         @endif
                                         <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true">
                                         </div>
@@ -46,15 +46,17 @@
                                 </div>
                             </td>
                             <td class="px-4 py-3 text-sm truncate">
-                                {{ strip_tags(substr(str_replace("&nbsp;", "", $pregnancy->content), 0, 70), null) }} ...
+                                {{ strip_tags(substr(str_replace('&nbsp;', '', $pregnancy->content), 0, 70), null) }}
+                                ...
                             </td>
                             <td class="px-4 text-center py-3 text-sm truncate">
-                              {{ $pregnancy->weeks }}
+                                {{ $pregnancy->weeks }}
                             </td>
                             <td class="px-4 py-3 text-sm flex nowrap">
                                 <a href="{{ route('admin.pregnancy_alerts.list.edit', $pregnancy) }}"
                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 mr-1 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</a>
-                                <button onclick="confirmDelete('{{ route('admin.pregnancy_alerts.list.destroy', $pregnancy) }}')"
+                                <button
+                                    onclick="confirmDelete('{{ route('admin.pregnancy_alerts.list.destroy', $pregnancy) }}')"
                                     class="delete-confirm focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1 mr-1 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                             </td>
                         </tr>
@@ -62,28 +64,27 @@
                 </tbody>
             </table>
         </div>
-    </div>
-    <script>
-        function confirmDelete(url) {
-            Swal.fire({
-                title: 'Apakah kamu yakun?',
-                text: 'Data ini akan dihapus secara permanen',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Ya, saya yakin'
-            }).then(function(value) {
-                if(value.isConfirmed) {
-                    window.location.href = url
-                }
-            })
-        }
+        <script>
+            function confirmDelete(url) {
+                Swal.fire({
+                    title: 'Apakah kamu yakun?',
+                    text: 'Data ini akan dihapus secara permanen',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Ya, saya yakin'
+                }).then(function(value) {
+                    if (value.isConfirmed) {
+                        window.location.href = url
+                    }
+                })
+            }
 
-        function pictureError(e, el) {
-          el.src = el.getAttribute('noimageshandler')
-        }
-    </script>
+            function pictureError(e, el) {
+                el.src = el.getAttribute('noimageshandler')
+            }
+        </script>
     </div>
     <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('js/tinymce/config.js') }}"></script>
